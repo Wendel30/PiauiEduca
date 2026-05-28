99f9335bd9f27742612a2665b55ef16983f90596
Wendel30 <wendelcarvalho2000@gmail.com>
2026-05-28 14:07:40 -0300

    Update breadcrumb.tsx

--- src/app/components/ui/breadcrumb.tsx
+++ src/app/components/ui/breadcrumb.tsx
@@ -1,3 +1,4 @@
+// Componente de navegação estrutural (Breadcrumb) - PiauiEduca+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { ChevronRight, MoreHorizontal } from "lucide-react";
