7d270968361a014a7a1fbbdc342b365844c7350d
Wendel30 <wendelcarvalho2000@gmail.com>
2026-05-28 14:09:36 -0300

    Update button.tsx

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,3 +1,4 @@
+// Componente de botão interativo (Button) - PiauiEducação+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
