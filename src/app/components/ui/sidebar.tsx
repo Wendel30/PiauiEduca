2ccb37a7d29f13c7ae1858ff6a476f6ff38e1810
Wendel30 <wendelcarvalho2000@gmail.com>
2026-05-28 15:08:54 -0300

    Update sidebar.tsx

--- src/app/components/ui/sidebar.tsx
+++ src/app/components/ui/sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// Componente de barra lateral
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
