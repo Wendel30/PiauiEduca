ccc2a4185b38095db7cf037abeef224c1a2d09a2
Wendel30 <wendelcarvalho2000@gmail.com>
2026-05-28 15:31:36 -0300

    Update use-mobile.ts

--- src/app/components/ui/use-mobile.ts
+++ src/app/components/ui/use-mobile.ts
@@ -2,6 +2,7 @@ import * as React from "react";
 
 const MOBILE_BREAKPOINT = 768;
 
+// Hook para verificar a responsividade mobile
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
     undefined,
