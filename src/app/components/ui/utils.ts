52c1f319d4d853bf2d341de01771e96ed8f12771
Wendel30 <wendelcarvalho2000@gmail.com>
2026-05-28 15:35:14 -0300

    Update utils.ts

--- src/app/components/ui/utils.ts
+++ src/app/components/ui/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Função utilitária para mesclar classes 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
